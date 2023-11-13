f3ac08136ecfe38072126376d22301db51531728
NicoaTheSix <nick5901879@gmail.com>
2023-11-13 17:42:16 +0800

    20231113

--- LIEF_TEST.py
+++ LIEF_TEST.py
@@ -9,6 +9,20 @@ print(binary)
 '''
 # PE
 binary = lief.parse(r"C:\\Users\\nick5\\output\\T\\Malware.exe")
-print(binary)
+print(binary.libraries)
 print(binary.imagebase)
 
+# Using the abstract layer
+for func in binary.imported_functions:
+  print(func)
+
+# Using the PE definition
+for func in binary.imports:
+  print(func)
+
+for imported_library in binary.imports:
+  print("Library name: " + imported_library.name)
+  for func in imported_library.entries:
+    if not func.is_ordinal:
+      print(func.name)
+    print(func.iat_address)
--- get_net.py
+++
@@ -1,4 +0,0 @@
-import os
-with os.popen(r"net user",'r') as text:
-    text_read=text.read()
-print(text_read.splitlines()[5].split(' ')[0])
